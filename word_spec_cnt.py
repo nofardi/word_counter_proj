7d8765f4d1630ce1234ed30cf6c480e7e2294578
Nofar Diamant <nofar1d@gmail.com>
2019-09-13 11:31:23 +0300

    remove old files

--- word_spec_cnt.py
+++
@@ -1,9 +0,0 @@
-"""
-Holds the logic behind counting the occerance of a specific word
-"""
-class WordSpecCnt():   
-    def __init__(self, word_count):
-        self.word_count = word_count
-
-    def count(self, word):
-        return self.word_count.words_dict[word.lower()]
